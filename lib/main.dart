1cdc28c1e730190a159810795cdc3497d4f5e92f
Jess <blancojetharjay@gmail.com>
2024-04-07 11:21:46 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.light();
+    return MaterialApp(
+      theme: theme,
+      scrollBehavior: CustomScrollBehavior(),
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  //Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices =>{
+  PointerDeviceKind.touch,
+  PointerDeviceKind.mouse,
+  // etc.
+};
+}
